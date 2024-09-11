fd473221440920f6b643faa22119953dcfd36f98
Bais26 <baisyufan2004@gmail.com>
2024-09-11 12:47:55 +0700

    init project,tailwind,reactdom

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  return (
+    <>
+    <div className="text-3xl font-bold text-purple-600">
+      hallo kawan
+    </div>
+    </>
+  )
+}
+
+export default App
