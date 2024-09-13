c579a6a7d893cb503defab22363a604e53af2dc8
Bais26 <baisyufan2004@gmail.com>
2024-09-14 06:27:32 +0700

    modify Responsive Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Logo from '../../../public/Netflix.png'
+import Logo from '../../../public/Netflix.png';
 
 interface NavItem {
     name: string;
@@ -9,79 +9,67 @@ interface NavItem {
 
 export default function Navbar() {
     const [toggle, setToggle] = useState(false);
-    const [showDropdown, setShowDropdown] = useState<Record<number, boolean>>({});
-    const navItems = [
+    const navItems: NavItem[] = [
         {
             name: "Home",
             url: "/",
         },
         {
             name: "Service",
-            url: "/service"
+            url: "/service",
         },
         {
             name: "About",
-            url: "/about"
+            url: "/about",
         },
         {
             name: "Blogs",
             url: "/blogs",
         },
         {
-            name: "Contacs",
-            url: "/contacs"
+            name: "Contacts",
+            url: "/contacts",
         },
-
     ];
 
     const handleToggleSidebar = () => {
         setToggle(!toggle);
     };
 
-    const handleToggleDropdown = (index: number) => {
-        setShowDropdown(prevState => ({
-            ...prevState,
-            [index]: !prevState[index]
-        }));
-    };
-
     return (
         <div className="container bg-transparent text-third sticky top-0 z-20">
             <div className="flex flex-wrap md:flex-nowrap justify-between p-4 items-center ">
-                    <a href="/" className=""><img src={Logo} alt="" /></a>
-                <div className="hidden md:flex items-center gap-12 md:gap-12 text-xl ">
-                    {navItems.map((items, index) => {
-                        return (
-                            <div className=" my-2" key={index}>
-                                <Link className="" key={index} to={items.url}>{items.name}</Link>
-                            </div>
-                        );
-                    })}
+                <a href="/" className=""><img src={Logo} alt="Logo" /></a>
+                <div className="hidden md:flex items-center gap-12 text-xl">
+                    {navItems.map((item, index) => (
+                        <Link key={index} to={item.url}>
+                            {item.name}
+                        </Link>
+                    ))}
                 </div>
-                <div className="md:hidden ">
+                {/* Mobile Menu Toggle Button */}
+                <div className="md:hidden">
                     <button onClick={handleToggleSidebar}>
-                        <i className=" text-2xl mt-2 fa-solid fa-bars "></i>
+                        <i className="text-2xl mt-2 fa-solid fa-bars"></i>
                     </button>
+                    {/* Sidebar for mobile */}
                     {toggle && (
-                        <div className="absolute w-full top-12 right-0 p-4 ">
-                            <div className="p-4 ">
-                                {navItems.map((items, index) => {
-                                    return (
-                                        <div className=" my-2" key={index}>
-                                            <span className=" " onClick={() => handleToggleDropdown(index)}>
-                                                <a className="" key={index} href={items.url}>{items.name}</a>
-                                            </span>
-                                        </div>
-                                    );
-                                })}
-                            </div>
+                        <div className="absolute w-full top-12 left-0 p-4 shadow-lg z-20">
+                            {navItems.map((item, index) => (
+                                <div className="my-2" key={index}>
+                                    <Link to={item.url} onClick={() => setToggle(false)}>
+                                        {item.name}
+                                    </Link>
+                                </div>
+                            ))}
                         </div>
                     )}
                 </div>
+                {/* Icons on desktop */}
                 <div className="text-xl hidden md:block">
-                <i className="mx-3 fa-solid fa-magnifying-glass"></i>
-                <i className="mx-3 fa-regular fa-bell"></i>
-                <i className="mx-3 fa-regular fa-user"></i>
+                    <i className="mx-3 fa-solid fa-magnifying-glass"></i>
+                    <i className="mx-3 fa-regular fa-bell"></i>
+                    <i className="mx-3 fa-regular fa-user"></i>
                 </div>
             </div>
         </div>
